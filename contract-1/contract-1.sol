bcfdbb749eb249eb7bf9242140c468968b4fc3da
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-08-17 22:33:03 +0300

    New!

--- contract-1/contract-1.sol
+++ contract-1/contract-1.sol
@@ -4,11 +4,13 @@ pragma solidity ^0.8.8; // 0.8.12
 
 contract SimpleStorage {
 // boolean, uint , int, address , bytes
-    bool hasFavoriteNumber = true;
-    uint256 favoriteNumber = 5;
-    string favoriteNumberInText = "Five";
-    int256 favoriteInt = -5;
-    address myAddress = 0x3Bc8f3277976012800ad1b3F494717ACf6b3F27e;
-    bytes32 favoriteBytes = "cat";
+    // bool hasFavoriteNumber = true;
+    // uint256 favoriteNumber = 5;
+    // string favoriteNumberInText = "Five";
+    // int256 favoriteInt = -5;
+    // address myAddress = 0x3Bc8f3277976012800ad1b3F494717ACf6b3F27e
     
+    //  This gets initialized to Zero!
+    // <- This means that this section is comment! 
+    uint256 favoriteNumber;
 }
---
+++ contract-1/contract-2-basic-functions.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8; // 0.8.12
+
+
+contract SimpleStorage {
+// 'Functions' or 'Methods' execute a subset of code when called 
+    uint256 favoriteNumber;
+}
