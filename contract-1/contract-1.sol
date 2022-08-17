c9da84f9b8e42438ca7fc4bb3406bcb85bf9040c
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-08-17 22:25:43 +0300

    Correction 👌

---
+++ contract-1/contract-1.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8; // 0.8.12
+
+
+contract SimpleStorage {
+// boolean, uint , int, address , bytes
+    bool hasFavoriteNumber = true;
+    uint256 favoriteNumber = 5;
+    string favoriteNumberInText = "Five";
+    int256 favoriteInt = -5;
+    address myAddress = 0x3Bc8f3277976012800ad1b3F494717ACf6b3F27e;
+    bytes32 favoriteBytes = "cat";
+    
+}
