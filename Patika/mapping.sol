50ff63e708f93d9efec9fd75e9b3f7380fdeffd5
furky.eth <52575351+furkananter@users.noreply.github.com>
2022-09-29 23:48:40 +0300

    Update mapping.sol

--- Patika/mapping.sol
+++ Patika/mapping.sol
@@ -6,5 +6,16 @@ pragma solidity ^0.8.13;
 // How to declare a mapping (simple and nested)
 // set, get and delete
 contract Mapping{
+    mapping(address => uint ) public balances;
+    mapping(address => mapping(address => bool)) public isFriend;
 
+    function example() external {
+        balances[msg.sender] = 123;
+        uint bal = balances[address(1)];
+        uint bal2 = balances[msg.sender];
+        
+        balances[msg.sender] += 456;
+        delete balances[msg.sender];
+        isFriend[msg.sender][address(this)];
+    }
 }
