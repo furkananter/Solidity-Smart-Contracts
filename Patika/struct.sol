ed6da0fc5dd5b94aef8c33fd193e422e022589fd
furky.eth <52575351+furkananter@users.noreply.github.com>
2022-10-01 15:20:10 +0300

    Update struct.sol

--- Patika/struct.sol
+++ Patika/struct.sol
@@ -1,9 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Struct{
-    struct Car{
+contract Struct {
+    struct Car {
         string model;
-        uint 
+        uint256 year;
+        address owner;
+    }
+    Car public car;
+    Car[] public cars;
+    mapping(address => Car[]) public carsByOwner;
+
+    function example() external {
+        Car memory bmw = Car("BMW", 2021, msg.sender);
+        Car memory lambo = Car({
+            model: "Lamborghini",
+            year: 2022,
+            owner: msg.sender
+        });
+        Car memory tesla;
+        tesla.model = "Tesla";
+        tesla.year = 2023;
+        tesla.owner = msg.sender;
+
+        cars.push(bmw);
+        cars.push(lambo);
+        cars.push(tesla);
+
+        cars.push(Car("Ferrari",2024,msg.sender));
+        Car memory _car = cars[0];
+        _car.model;
+        _car.year;
+        _car.owner;
+        
     }
 }
