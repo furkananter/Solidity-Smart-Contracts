8abc70d89315f9270b824d5038384e67a6374239
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-10-06 14:55:17 +0300

    Update Call.sol

--- Patika/Call.sol
+++ Patika/Call.sol
@@ -1,8 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+contract TestCall {
+    string public message;
+    uint public x;
+    event Log(string message);
+
+    fallback() external payable {
+        emit Log("Fallback called");
+    }
+
+    function foo(string memory _message, uint _x) external payable returns(bool, uint) {
+        message = _message;
+        x = _x;
+        return(true, 999);
+    }
+}
+
 contract Call {
-    
+    function testFoo(address _test ) external {
+        
+    }
 }
 
 
